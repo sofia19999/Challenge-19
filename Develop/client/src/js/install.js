3c1d82e031911e9b7e26243d8b6667c2285e2bc9
sofia19999 <kaminskisofia9@gmail.com>
2024-09-30 13:23:43 -0400

    changes

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,46 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
+
+
+
+
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    
+    event.preventDefault();
+  
+    deferredPrompt = event;
+    
+    butInstall.style.display = 'block';
+    console.log('👍', 'beforeinstallprompt event fired');
+  });
+  
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+    butInstall.style.display = 'none';
+    if (deferredPrompt) {
+ 
+      deferredPrompt.prompt();
+
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+ 
+      deferredPrompt = null;
+    }
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled event fired');
+  
+    deferredPrompt = null;
+  });
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,30 +1,65 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+module.exports = {
+  mode: 'development', 
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Text Editor',
+    }),
 
-    module: {
-      rules: [
-        
+   
+    new InjectManifest({
+      swSrc: './src-sw.js', // Custom service worker source file
+      swDest: 'src-sw.js',  // Destination in the output (dist) folder
+    }),
+
+    // Generates the manifest.json file for the PWA
+    new WebpackPwaManifest({
+      name: 'Text Editor',
+      short_name: 'JATE',
+      description: 'Just Another Text Editor!',
+      background_color: '#ffffff',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './', // Ensure relative path for deployment
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'), // Path to the source icon in the src/images folder
+          sizes: [96, 128, 192, 256, 384, 512],     // Multiple sizes for different devices
+          destination: path.join('assets', 'icons'), // Copy to 'dist/assets/icons'
+        },
       ],
-    },
-  };
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
